de13e8a27b524b756e17e1bfeebbe30be9eae609
ahn <dudska0202@naver.com>
2023-11-16 00:07:54 +0900

    알림 시스템 연월일 분리해서 쓰기 편하게 만듦 날짜 체크해서 비정상적인 날짜는 다시 입력하게 만듦 주행거리 오류난거 일단 임시로 메꿔놔서 테스트 가능하게 만듦

--- app/src/main/java/com/example/myapplication/Distance_cal.java
+++ app/src/main/java/com/example/myapplication/Distance_cal.java
@@ -16,9 +16,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import android.app.AlertDialog;
+import android.widget.DatePicker;
+import java.util.Calendar;
 
 public class Distance_cal extends AppCompatActivity {
-    private EditText replacementDateEditText;
+
     private Button saveButton;
     private DBHelper dbHelper;
     private Spinner partSpinner;
@@ -26,6 +29,9 @@ public class Distance_cal extends AppCompatActivity {
     private ListView carPartsListView;
     private CarPartsAdapter carPartsAdapter;
     private List<DBHelper.RecommendedReplacement> recommendedReplacements;
+    // 연, 월, 일 EditText 필드
+    private EditText yearEditText, monthEditText, dayEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +48,7 @@ public class Distance_cal extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.imageView);
         Button viewRecommendedButton = findViewById(R.id.viewRecommendedButton);
         Button saveButton = findViewById(R.id.saveButton);
-
+        // 연, 월, 일 EditText 필드
         viewRecommendedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,8 +67,10 @@ public class Distance_cal extends AppCompatActivity {
         });
 
         // UI 요소 초기화
+        yearEditText = findViewById(R.id.yearEditText);
+        monthEditText = findViewById(R.id.monthEditText);
+        dayEditText = findViewById(R.id.dayEditText);
         partSpinner = findViewById(R.id.partSpinner);
-        replacementDateEditText = findViewById(R.id.replacementDateEditText);
         mileageSpinner = findViewById(R.id.mileageSpinner);
         saveButton = findViewById(R.id.saveButton);
         carPartsListView = findViewById(R.id.carPartsListView);
@@ -73,11 +81,16 @@ public class Distance_cal extends AppCompatActivity {
         ArrayAdapter<String> partAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, partList);
         partAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         partSpinner.setAdapter(partAdapter);
+        if (partAdapter.getCount() > 0) {
+            partSpinner.setSelection(0);
+        }
 
         // 주행 거리 목록 설정
-        List<String> mileageList = new ArrayList<>();
-        mileageList.add("10");
-        mileageList.add("20");
+        mileageSpinner = findViewById(R.id.mileageSpinner);
+        ArrayAdapter<String> mileageAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
+                new String[]{"0", "20", "50", "100", "150", "200", "500"});
+        mileageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mileageSpinner.setAdapter(mileageAdapter);
         // 나머지 주행 거리 추가
 
         // CarPartsAdapter를 ListView에 연결
@@ -85,32 +98,48 @@ public class Distance_cal extends AppCompatActivity {
         carPartsListView.setAdapter(carPartsAdapter);
 
         // "저장" 버튼 클릭 이벤트 리스너 설정
+
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 사용자가 입력한 데이터 가져오기
-                String partName = partSpinner.getSelectedItem().toString();
-                String replacementDate = replacementDateEditText.getText().toString();
-                String mileage = mileageSpinner.getSelectedItem().toString();
 
-                // 데이터베이스에 데이터 저장 또는 업데이트
-                long result = dbHelper.insertOrUpdateCarPart(partName, mileage, replacementDate);
+                try {
 
-                if (result != -1) {
-                    // 데이터가 성공적으로 저장되거나 업데이트됨
-                    Toast.makeText(Distance_cal.this, "데이터가 저장되었습니다.", Toast.LENGTH_SHORT).show();
-                    // 저장 후, 목록 업데이트
-                    carPartsAdapter.clear();
-                    carPartsAdapter.addAll(dbHelper.getAllCarParts());
-                    // ListView를 업데이트한 후, 스크롤하여 최신 데이터를 볼 수 있도록 이동
-                    carPartsListView.smoothScrollToPosition(carPartsAdapter.getCount() - 1);
+                    // 사용자 입력을 정수로 변환
+                    int year = Integer.parseInt(yearEditText.getText().toString());
+                    int month = Integer.parseInt(monthEditText.getText().toString()) - 1; // Calendar는 월을 0부터 시작
+                    int day = Integer.parseInt(dayEditText.getText().toString());
 
-                    // 다음 교체 예정 주행 거리 계산
-                    int nextReplacementDistance = calculateNextReplacementDistance(replacementDate, partName);
-                    Toast.makeText(Distance_cal.this, "다음 교체 예정 주행 거리: " + nextReplacementDistance + " km", Toast.LENGTH_SHORT).show();
-                } else {
-                    // 데이터 저장 중 오류 발생
-                    Toast.makeText(Distance_cal.this, "데이터 저장 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                    // Calendar를 사용하여 날짜 유효성 검사
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setLenient(false);
+                    calendar.set(year, month, day);
+                    calendar.getTime(); // 유효하지 않은 날짜면 여기서 예외가 발생
+
+                    // 데이터베이스에 저장할 날짜 형식으로 변환
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
+                    String formattedDate = dateFormat.format(calendar.getTime());
+
+                    // 데이터베이스 저장 로직 추가
+                    String partName = partSpinner.getSelectedItem().toString();
+                    String mileage = mileageSpinner.getSelectedItem().toString();
+                    long result = dbHelper.insertOrUpdateCarPart(partName, mileage, formattedDate);
+
+                    if (result != -1) {
+                        Toast.makeText(Distance_cal.this, "데이터가 저장되었습니다.", Toast.LENGTH_SHORT).show();
+                        // ListView 업데이트
+                        carPartsAdapter.clear();
+                        carPartsAdapter.addAll(dbHelper.getAllCarParts());
+                        carPartsListView.smoothScrollToPosition(carPartsAdapter.getCount() - 1);
+                    } else {
+                        Toast.makeText(Distance_cal.this, "데이터 저장 실패.", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (NumberFormatException e) {
+                    Toast.makeText(Distance_cal.this, "날짜 형식이 올바르지 않습니다.", Toast.LENGTH_SHORT).show();
+                } catch (IllegalArgumentException e) {
+                    Toast.makeText(Distance_cal.this, "유효하지 않은 날짜입니다.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
